891af08f0062012edc0b52e8af72cae908d037b3
Tim O'Farrell <tofarr@everyonesocial.com>
2019-05-10 22:12:29 -0600

    Removed todo

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,7 +2,6 @@ require "ez_crud/route"
 
 Rails.application.routes.draw do
 
-  resources :users
   EzCrud::Route.resource(self, :comments, :categories, :doohickeys, :users)
 
   #mount EzCrud::Engine => "/ez_crud"
